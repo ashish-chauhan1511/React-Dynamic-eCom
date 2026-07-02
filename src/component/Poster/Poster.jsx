9ca307e5d35c51160cefad4e5622f2a33b30f9e2
ashish-chauhan1511 <ashishchauhan.mail30@gmail.com>
2026-07-02 10:35:16 +0530

    Merge branch 'main' of https://github.com/ashish-chauhan1511/React-Dynamic-eCom

--- src/component/Poster/Poster.jsx
+++ src/component/Poster/Poster.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Poster =({title, pimage})=>{
     return(
       <div className="poster" style={{backgroundImage: `url(${pimage})`}}>
-     {/* <h1>{title}</h1> */}
+   
       </div>  
     );
 };
